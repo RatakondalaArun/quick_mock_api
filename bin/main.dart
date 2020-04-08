dc9fdb3db8303a6eaf899ea79523e8a13927c926
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-04-08 11:10:52 +0530

    updated port

--- bin/main.dart
+++ bin/main.dart
@@ -3,10 +3,11 @@ import 'dart:io';
 import 'routes.dart';
 
 main() async {
-  const port = 8080;
-  final InternetAddress address = InternetAddress.loopbackIPv6;
+  final port = Platform.environment['PORT'] ?? '8080';
 
-  HttpServer requestServer = await HttpServer.bind(address, port);
+  final InternetAddress address = InternetAddress.loopbackIPv4;
+
+  HttpServer requestServer = await HttpServer.bind(address, int.parse(port));
   print('server is running at port ${requestServer.port}');
   await for (HttpRequest request in requestServer) {
     ServerRoutes routes = ServerRoutes();
