8a9e1de65deb8f0fa3e5435bdc2b06cfc86bb9c5
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-04-08 11:54:22 +0530

    updated logs and internet address

--- bin/main.dart
+++ bin/main.dart
@@ -5,12 +5,16 @@ import 'routes.dart';
 main() async {
   final port = Platform.environment['PORT'] ?? '8080';
 
-  final InternetAddress address = InternetAddress.loopbackIPv4;
+  final InternetAddress address = InternetAddress.anyIPv4;
 
   HttpServer requestServer = await HttpServer.bind(address, int.parse(port));
-  print('server is running at port ${requestServer.port}');
+  print('Dart server is running at port ${requestServer.port}');
   await for (HttpRequest request in requestServer) {
+    print(
+        'recived a request-------------------------------------------------------');
     ServerRoutes routes = ServerRoutes();
     routes.send(request);
+    print(
+        'request ---------------------------END------------------------------');
   }
 }
